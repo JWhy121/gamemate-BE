713c81ab8c0c11ac4f127fe9b028a304925ac2f0
[Cloud3] 김연지 <yjuio8357@naver.com>
2024-07-22 21:40:27 +0900

    Initial commit

---
+++ src/main/java/com/example/gamemate/GamemateApplication.java
@@ -0,0 +1,13 @@
+package com.example.gamemate;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GamemateApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GamemateApplication.class, args);
+	}
+
+}
