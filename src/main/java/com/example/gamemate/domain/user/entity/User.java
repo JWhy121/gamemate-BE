d6a36475f9128c6752704fe55157a49482a3963c
[Cloud3] 고의성 <keskwh@naver.com>
2024-08-09 15:01:46 +0900

    Feat:password nullable 설정

--- src/main/java/com/example/gamemate/domain/user/entity/User.java
+++ src/main/java/com/example/gamemate/domain/user/entity/User.java
@@ -31,7 +31,7 @@ public class User extends BaseEntity {
     @Email(message = "이메일 형식이 유효하지 않습니다.")
     private String username;
 
-    @Column(nullable = false, length = 100)
+    @Column(nullable = true, length = 100)
     private String password;
 
     @Column(nullable = false, length = 20)
