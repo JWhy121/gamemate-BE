b64c07b8c760e138c2848b03780d6cba4fb98aaa
[Cloud3] 김이삭 <xovnd7845@naver.com>
2024-08-14 15:57:01 +0900

    feat 기능 거의 마무리 수정

--- src/main/java/com/example/gamemate/domain/chat/dto/ChatRoomDTO.java
+++ src/main/java/com/example/gamemate/domain/chat/dto/ChatRoomDTO.java
@@ -8,11 +8,13 @@ public class ChatRoomDTO {
     private String title;
     private String leaderNickname;
     private Long memberCnt;
+    private String leaderProfile;
 
-    public ChatRoomDTO(Long id, String title, String leaderNickname, Long memberCnt) {
+    public ChatRoomDTO(Long id, String title, String leaderNickname, Long memberCnt, String leaderProfile) {
         this.id = id;
         this.title = title;
         this.leaderNickname = leaderNickname;
         this.memberCnt = memberCnt;
+        this.leaderProfile = leaderProfile;
     }
 }
